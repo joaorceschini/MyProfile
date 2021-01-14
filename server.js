3dbd26756401060bfa32ba3670d0ef3fe2952fc4
joaorceschini <jrvceschini@gmail.com>
2021-01-14 18:25:05 -0300

    ok

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express()
+const path = require('path')
+
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, "public/views/index.html"))
+})
+
+server.listen(3000, () => {
+  console.log("[SERVER]・Servidor rodando na porta 3000")
+})
